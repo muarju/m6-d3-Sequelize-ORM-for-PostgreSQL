90b693a022f0dfdd8d6b5630bc10e5b3cfbdc3a2
Engr. Md. Muslim Uddin Arju <mdarju240@gmail.com>
2021-08-26 14:24:49 +0100

    Comment Route for CRUD

---
+++ src/services/comment/index.js
@@ -0,0 +1,71 @@
+import express from "express";
+import db from "../../db/models/index.js";
+const Comment = db.Comment;
+import s from "sequelize";
+const { Op } = s;
+
+const router = express.Router();
+
+router
+  .route("/")
+  .get(async (req, res, next) => {
+    try {
+      const data = await Comment.findAll();
+      res.send(data);
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  })
+  .post(async (req, res, next) => {
+    try {
+      const data = await Comment.create(req.body);
+      res.send(data);
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  });
+
+router
+  .route("/:id")
+  .get(async (req, res, next) => {
+    try {
+      const data = await Comment.findByPk(req.params.id);
+      res.send(data);
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  })
+  .put(async (req, res, next) => {
+    try {
+      const data = await Comment.update(req.body, {
+        where: { id: req.params.id },
+        returning: true,
+      });
+      res.send(data[1][0]);
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  })
+  .delete(async (req, res, next) => {
+    try {
+      const rows = await Comment.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
+      if (rows > 0) {
+        res.send("Deleted");
+      } else {
+        res.status(404).send("not found");
+      }
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  });
+
+export default router;
