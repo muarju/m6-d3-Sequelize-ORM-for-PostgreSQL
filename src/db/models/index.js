a30ab704aefa59e048bf0d7eed4c1a14fe44163e
Engr. Md. Muslim Uddin Arju <mdarju240@gmail.com>
2021-08-26 14:11:01 +0100

    associations between the Comment, User, Products.

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -1,8 +1,13 @@
 import Category from "./Category.js";
 import Product from "./Product.js";
+import User from "./User.js";
+import Comment from "./comment.js";
 import sequelize from "../index.js";
 
 Product.belongsTo(Category);
 Category.hasMany(Product);
+Comment.belongsTo(User)
+User.hasMany(Comment)
+Product.hasMany(Comment)
 
-export default { Category, sequelize, Product };
+export default { Category, sequelize, Product,User,Comment };
